3670bf619b537896b2469a2f74592bbb1c8b0589
DenerThiago21 <denerthi.ibi@gmail.com/>
2022-07-04 21:50:45 -0300

    desafio de codigo frontend Angular

---
+++ src/app/components/bookstore-app/product-list/model/Book.ts
@@ -0,0 +1,18 @@
+/**
+ * API class Product
+ * String id
+ * String name
+ * double price
+ * int quantity
+ * String category
+ * String img
+ */
+
+export class Book {
+  id!: string;
+  name!: string;
+  price!: number;
+  quatity!: number;
+  category!: string;
+  img!: string;
+}
